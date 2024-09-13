300b4b7af67a91f326d27ab4870eba123739b4c3
Bradford Derby <derby.b@northeastern.edu>
2024-09-13 10:34:32 -0400

    add subtract operation

---
+++ dist/calculator.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Calculator = void 0;
+class Calculator {
+    add(x, y) {
+        return x + y;
+    }
+    subtract(x, y) {
+        return x - y;
+    }
+}
+exports.Calculator = Calculator;
+//# sourceMappingURL=calculator.js.map
